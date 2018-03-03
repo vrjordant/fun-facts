390e9ab2fc880ee0a98f73cab823f7814e5e26df
Jordan <vrjordant@gmail.com>
2018-03-03 11:43:01 -0500

    finalized first batch of facts

--- fun-facts.cpp
+++ fun-facts.cpp
@@ -21,8 +21,17 @@ int main() {
 	facts.push_back("My favorite musician is insaneintherainmusic");
 	facts.push_back("I'm planning on making a Pokemon game based on the CS courses I've taken at Stevens");
 	facts.push_back("I'm left-handed");
-	facts.push_back("My favorite quote is 'Everyone you'll ever meet knows something you don't' by Bill Nye.");
+	facts.push_back("My favorite quote is 'Everyone you'll ever meet knows something you don't' by Bill Nye");
 	facts.push_back("My favorite video game is Destiny");
 	facts.push_back("I programmed a Pokemon text-adventure in Java");
 	facts.push_back("So far, my favorite movie of 2018 is Black Panther");
+	facts.push_back("I am currently a CA for CS 385, Algorithms");
+	facts.push_back("This summer, I will be working at Northrop Grumman in McLean, VA for a summer internship");
+	facts.push_back("Last semester, I worked as a tutor for the Academic Support Center");
+	facts.push_back("My New Year's resolution for 2018 is to be able to touch my toes (it was also last year's too)");
+	facts.push_back("My favorite anime is Haikyu!!");
+	facts.push_back("My favorite YouTube video is https://www.youtube.com/watch?v=dQw4w9WgXcQ :)");
+	facts.push_back("Actually, my real favorite YouTube video is https://www.youtube.com/watch?v=aT8ix3ZNlLM");
+
+	
 }
